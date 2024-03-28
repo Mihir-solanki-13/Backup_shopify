05e7559a38989712ab3fcfd15d5102e7460a0228
mihir singh <13mihir2001@gmail.com>
2024-03-28 22:43:42 +0530

    model added to store product

--- objects/models.py
+++ objects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class MyModel(models.Model):
+    json_data = models.JSONField()  # Field to store JSON data
+    version = models.CharField(max_length=255)  # Character field
+    created_at = models.DateTimeField(default=timezone.now)  # Current date/time
+
+     
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+If anyone face migration issue with JSONField in sqlite 
+refer:https://stackoverflow.com/questions/62637458/django-3-1-fields-e180-sqlite-does-not-support-jsonfields
